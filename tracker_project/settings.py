a590b7117979b2d3c1b760677d536e3c2bdf5c68
Bhavish <bhavishmulleshwari@gmail.com>
2026-03-23 14:00:46 +0530

    fix static root error

--- tracker_project/settings.py
+++ tracker_project/settings.py
@@ -66,6 +66,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
      'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -144,6 +145,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 PROTECTED_MEDIA_ROOT = BASE_DIR / "protected_media"
@@ -219,5 +225,5 @@ LOGGING = {
         },
     },
 }
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 NOTIFICATION_DAYS_BEFORE = [30, 7, 1]
